1e19d7ef439a52dbfb5aa99a2340b1ecec19749c
Dale Gacusan <dale@xpay.world>
2024-08-27 20:58:58 +0800

    Added meta tag image

--- pages/[username]/index.tsx
+++ pages/[username]/index.tsx
@@ -232,6 +232,30 @@ export default function Home() {
         <meta name='robots' content='index, follow' />
         <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
         <meta name='language' content='English' />
+
+        <meta property='og:type' content='website' />
+        <meta property='og:url' content='https://taptech.ph/' />
+        <meta property='og:title' content='Tap Technologies' />
+        <meta
+          property='og:description'
+          content='Tap into the future and revolutionize the way you share your business and contact information by using an environmentally friendly alternative business card'
+        />
+        <meta
+          property='og:image'
+          content='/images/logo.png'
+        />
+
+        <meta property='twitter:card' content='summary_large_image' />
+        <meta property='twitter:url' content='https://taptech.ph/' />
+        <meta property='twitter:title' content='Tap Technologies' />
+        <meta
+          property='twitter:description'
+          content='Tap into the future and revolutionize the way you share your business and contact information by using an environmentally friendly alternative business card'
+        />
+        <meta
+          property='twitter:image'
+          content='/images/logo.png'
+        />
       </Head>
 
       <main style={backgroundStyle}>
--- pages/index.tsx
+++ pages/index.tsx
@@ -16,6 +16,30 @@ export default function HomePage() {
         <meta name='robots' content='index, follow' />
         <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
         <meta name='language' content='English' />
+
+        <meta property='og:type' content='website' />
+        <meta property='og:url' content='https://taptech.ph/' />
+        <meta property='og:title' content='Tap Technologies' />
+        <meta
+          property='og:description'
+          content='Tap into the future and revolutionize the way you share your business and contact information by using an environmentally friendly alternative business card'
+        />
+        <meta
+          property='og:image'
+          content='/images/logo.png'
+        />
+
+        <meta property='twitter:card' content='summary_large_image' />
+        <meta property='twitter:url' content='https://taptech.ph/' />
+        <meta property='twitter:title' content='Tap Technologies' />
+        <meta
+          property='twitter:description'
+          content='Tap into the future and revolutionize the way you share your business and contact information by using an environmentally friendly alternative business card'
+        />
+        <meta
+          property='twitter:image'
+          content='/images/logo.png'
+        />
       </Head>
 
       <div
@@ -30,9 +54,14 @@ export default function HomePage() {
           alignItems: 'center',
         }}
       >
-        <Grid container justifyContent='center' alignItems='center' style={{ textAlign: 'center' }}>
+        <Grid
+          container
+          justifyContent='center'
+          alignItems='center'
+          style={{ textAlign: 'center' }}
+        >
           <Grid item>
-            <img src='/images/logo.png' alt='logo' style={{width: '10%'}}/>
+            <img src='/images/logo.png' alt='logo' style={{ width: '10%' }} />
           </Grid>
         </Grid>
       </div>
